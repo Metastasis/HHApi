3e5c329059d67fca01c9f9223509c5549b8a41a0
Metastasis <d1mq44@gmail.com>
2017-02-04 19:51:57 +0300

    add hh parsing

--- app/controllers/headhunter_controller.rb
+++ app/controllers/headhunter_controller.rb
@@ -19,12 +19,75 @@ class HeadhunterController < ApplicationController
       token = @client.auth_code.get_token(@code)
       token.client.site = api_site
       session['current_token'] = token.to_hash
-    elsif session['current_token']
-      @client.site=api_site
-      token = OAuth2::AccessToken.from_hash(@client, session['current_token'])
-      @response = token.get('/me', :headers => { 'User-Agent' => ua })
     end
 
+    if session['current_token']
+      @client.site=api_site
 
+      token = OAuth2::AccessToken.from_hash(@client, session['current_token'])
+      if token.expired?
+        token = token.refresh!
+      end
+
+      @r = token.get('/me', :headers => { 'User-Agent' => ua }).parsed
+
+      @users = populateUsers(token)
+    end
   end
+
+  def getResumeHtml()
+    url = 'https://hh.ru/search/resume?age_to=30&order_by=publication_time&specialization=17&schedule=fullDay&text=Teletrade&area=1&pos=full_text&label=only_with_age&exp_period=all_time&logic=normal&age_from=20&saved_search_id=1846440&employment=full&isAutosearch=true'
+    html = open(url)
+    # html = open(url, {ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE})
+    doc = Nokogiri::HTML(html)
+
+    return doc
+  end
+
+  def getUserInfo(doc)
+    users = []
+    doc.css('a.output__name').each do |link|
+      resume_id = link['href'].split('/')[2]
+      resume_id = resume_id[/[^\?]+/]
+      users.push(link: link['href'], resume_id: resume_id)
+    end
+
+    return users
+  end
+
+  def populateUsers(token)
+    ua = "Sender/1.0 (filimonov@stforex.com)"
+    doc = getResumeHtml()
+    users = getUserInfo(doc)
+
+    result = []
+    users.each do |user|
+      resume_url = '/resumes/' + user[:resume_id].to_s
+      response = token.get(resume_url, :headers => { 'User-Agent' => ua }).parsed
+      result.push(response)
+    end
+
+    users = []
+    result.each do |response|
+      firstname = response['first_name']
+      lastname = response['last_name']
+      phone = ''
+      email = ''
+
+      response['contact'].each do |contact|
+        if contact['type']['id'] == 'cell'
+          phone = contact['value']
+        end
+
+        if contact['type']['id'] == 'email'
+          email = contact['value']
+        end
+      end
+
+      users.push(firstname:firstname, lastname:lastname, email:email, phone:phone)
+    end
+
+    return users
+  end
+
 end
