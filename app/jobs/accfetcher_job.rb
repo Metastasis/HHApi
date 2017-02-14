1fed3d139bf12932ca22135f4d77a3e31d3501e5
Metastasis <d1mq44@gmail.com>
2017-02-14 15:02:42 +0300

    Now we are handling mailer errors

--- app/jobs/accfetcher_job.rb
+++ app/jobs/accfetcher_job.rb
@@ -2,11 +2,18 @@ class AccfetcherJob
   include SuckerPunch::Job
 
   def perform
-    subscribers = Subscriber.where(isNotified:[nil, false]).limit(80)
+    subscribers = Subscriber.where(isNotified: false).limit(80)
     subscribers.each do |user|
-      user.isNotified = true
-      user.save
-      SenderMailer.email(user).deliver
+      begin
+        SenderMailer.email(user).deliver
+        user.isNotified = true
+        user.save
+      rescue Net::SMTPAuthenticationError, Net::SMTPServerBusy, Net::SMTPSyntaxError, Net::SMTPFatalError, Net::SMTPUnknownError => e
+        Rails.logger.info "Can not send email to " + user.email
+        Rails.logger.info e.inspect
+      end
     end
+
   end
+
 end
